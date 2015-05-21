a26c0a3919ef846ea019a7bc14fb70754ba97b40
davidMGS <david.ho@mountaingapsolutions.com>
2015-05-21 15:39:47 -0400

    Added jscs, fixed jscs errors

--- src/app/app.js
+++ src/app/app.js
@@ -3,24 +3,24 @@
  * See styleguide @ https://github.com/toddmotto/angularjs-styleguide
  */
 (function() {
-    angular.module('boilerplate-app', [
-        'ionic',
-        'ngCordova',
-        'templates-app',
-        'templates-common',
+  angular.module('boilerplate-app', [
+      'ionic',
+      'ngCordova',
+      'templates-app',
+      'templates-common',
 
-        // Insert modules here
-        'boilerplate-app.home'
-    ])
+      // Insert modules here
+      'boilerplate-app.home'
+  ])
 
     .run(function($ionicPlatform, $cordovaSplashscreen) {
-        $ionicPlatform.ready(function() {
-            $cordovaSplashscreen.hide();
-        });
+      $ionicPlatform.ready(function() {
+        $cordovaSplashscreen.hide();
+      });
     })
 
     .config(['$urlRouterProvider', '$locationProvider', function($urlRouterProvider, $locationProvider) {
-        $urlRouterProvider.otherwise("/home");
+      $urlRouterProvider.otherwise('/home');
     }])
 
     .controller('AppController', function($scope) {
